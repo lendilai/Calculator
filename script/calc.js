de48ec479ffe4cc64442c5f706cedafe854a1d6a
lendilai <ngethenan768@gmail.com>
2019-04-24 11:14:22 +0300

    add js for subtraction

--- script/calc.js
+++ script/calc.js
@@ -1,7 +1,7 @@
-var add = function(number1, number2){
-	return number1 + number2;
+var subtract = function(number1, number2){
+	return number1 - number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var result = subtract(number1, number2);
 alert(result);
